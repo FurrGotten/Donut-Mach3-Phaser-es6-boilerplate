a3fedd665c35ae3a542145e46dfd3f315cf95afa
FurrG <A2674151a>
2020-01-10 17:27:13 +0200

    font add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+import * as WebFont from 'webfontloader';
+
 import GameState from 'states/GameState';
 
 class Game extends Phaser.Game {
 
 	constructor() {
 		super(500, 500, Phaser.AUTO, 'content', null);
+		WebFont.load({
+			google: {
+				families: ['Fredoka One']
+			}
+		});
 		this.state.add('GameState', GameState, false);
 		this.state.start('GameState');
 	}
