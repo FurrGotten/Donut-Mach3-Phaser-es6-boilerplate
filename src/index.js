90ce1765f3235bf5af238acd070bd1b30120ad26
FurrG <A2674151a>
2020-01-11 16:33:46 +0200

    state add

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import * as WebFont from 'webfontloader';
 
-import GameState from 'states/GameState';
-
+import mainMenuState from 'states/mainMenuState.js';
+import gamePlatformState from 'states/gamePlatformState.js';
+import endGameStateWin from 'states/endGameStateWin.js';
+import endGameStateLose from 'states/endGameStateLose.js';
 class Game extends Phaser.Game {
 
 	constructor() {
-		super(500, 500, Phaser.AUTO, 'content', null);
+		super(1280, 1150, Phaser.AUTO, 'content', null);
 		WebFont.load({
 			google: {
 				families: ['Fredoka One']
 			}
 		});
-		this.state.add('GameState', GameState, false);
-		this.state.start('GameState');
+		this.state.add('mainMenuState', mainMenuState, false);
+		this.state.add('gamePlatformState', gamePlatformState, false);
+		this.state.add('endGameStateWin', endGameStateWin, false);
+		this.state.add('endGameStateLose', endGameStateLose, false);
+		this.state.start('mainMenuState');
 	}
 
 }
---
+++ src/states/gamePlatformState.js
@@ -0,0 +1,37 @@
+import {centerX, centerY, changeState} from "./common";
+
+
+class gamePlatformState extends Phaser.State {
+    preload() {
+        this.load.image('background', 'assets/images/backgrounds/background.jpg');
+        this.load.image('scoreTable', 'assets/images/bg-score.png');
+        this.load.image('gem-01', 'assets/images/game/gem-01.png');
+        this.load.image('gem-02', 'assets/images/game/gem-02.png');
+        this.load.image('gem-03', 'assets/images/game/gem-03.png');
+        this.load.image('gem-04', 'assets/images/game/gem-04.png');
+        this.load.image('gem-05', 'assets/images/game/gem-05.png');
+        this.load.image('gem-06', 'assets/images/game/gem-06.png');
+        this.load.image('gem-07', 'assets/images/game/gem-07.png');
+        this.load.image('gem-08', 'assets/images/game/gem-08.png');
+        this.load.image('gem-09', 'assets/images/game/gem-09.png');
+        this.load.image('gem-10', 'assets/images/game/gem-10.png');
+        this.load.image('gem-11', 'assets/images/game/gem-11.png');
+        this.load.image('gem-12', 'assets/images/game/gem-12.png');
+        this.load.image('hand', 'assets/images/game/hand.png');
+    }
+    create() {
+        this.stage.backgroundColor = '#fffcad';
+        let backgroundImage = this.add.sprite(0, 190, 'background');
+        let scoreTable = this.add.sprite( centerX - 200, 10, 'scoreTable');
+        scoreTable.anchor.setTo(0.5, 0);
+        this.time.events.add(Phaser.Timer.SECOND * 5, lose, this);
+    }
+    update() {
+    }
+    render() {
+    }
+}
+function lose() {
+    changeState.call(this, null, 'endGameStateLose');
+}
+export default gamePlatformState;
