b2b050e7381e8b86a0f506236d6550e564722361
FurrG <A2674151a>
2020-01-15 23:23:27 +0200

    add sessionStorage state system 2

--- src/index.js
+++ src/index.js
@@ -4,34 +4,38 @@ import GamePlatformState from 'states/GamePlatformState.js';
 import EndGameStateWin from 'states/EndGameStateWin.js';
 import EndGameStateLose from 'states/EndGameStateLose.js';
 import TutorialState from 'states/TutorialState.js';
+
 class Game extends Phaser.Game {
 
-	constructor() {
-		super(1090, 1100, Phaser.AUTO, 'content', null);
-		WebFont.load({
-			google: {
-				families: ['Fredoka One']
-			}
-		});
-		this.state.add('MainMenuState', MainMenuState, false);
-		this.state.add('GamePlatformState', GamePlatformState, false);
-		this.state.add('EndGameStateWin', EndGameStateWin, false);
-		this.state.add('EndGameStateLose', EndGameStateLose, false);
-		this.state.add('TutorialState', TutorialState, false);
-		if (sessionStorage.getItem('curState') === 'MainMenuState'){
-			this.state.start('MainMenuState');
-		} else if (sessionStorage.getItem('curState') === 'GamePlatformState'){
-			this.state.start('GamePlatformState');
-		} else if (sessionStorage.getItem('curState') === 'EndGameStateWin'){
-			this.state.start('EndGameStateWin');
-		} else if (sessionStorage.getItem('curState') === 'EndGameStateLose'){
-			this.state.start('EndGameStateLose');
-		} else if (sessionStorage.getItem('curState') === 'TutorialState'){
-			this.state.start('TutorialState');
-		} else {
-			this.state.start('MainMenuState');
-		}
-	}
+    constructor() {
+        super(1090, 1100, Phaser.AUTO, 'content', null);
+        WebFont.load({
+            google: {
+                families: ['Fredoka One']
+            }
+        });
+        this.state.add('MainMenuState', MainMenuState, false);
+        this.state.add('GamePlatformState', GamePlatformState, false);
+        this.state.add('EndGameStateWin', EndGameStateWin, false);
+        this.state.add('EndGameStateLose', EndGameStateLose, false);
+        this.state.add('TutorialState', TutorialState, false);
+        switch (sessionStorage.getItem('curState')) {
+            case 'GamePlatformState':
+                this.state.start('GamePlatformState');
+                break;
+            case 'EndGameStateWin':
+                this.state.start('EndGameStateWin');
+                break;
+            case 'EndGameStateLose':
+                this.state.start('EndGameStateLose');
+                break;
+            case 'TutorialState':
+                this.state.start('TutorialState');
+                break;
+            default:
+                this.state.start('MainMenuState');
+        }
+    }
 }
 
 new Game();
